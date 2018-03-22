465b8e88c14c712be7cc32aa7f1ca1be58f3fb39
tutenghaojiao <473995726@qq.com>
2018-03-22 08:34:24 +0800

    2018年3月22日08:33:05

---
+++ app/home/controller/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *description		 文章管理控制器测试类
+ *FILE_NAME          zxy_frame
+ *author             周天君
+ *date               2018/3/21 17:24:00
+ */
+
+namespace app\home\controller;
+
+
+class ArticleController
+{
+public function add(){
+	echo '我是辅助测试app home controller 的一个 articlecontroller 类中的方法add';
+}
+	public function  edit(){
+		echo '我是辅助测试app home controller 的一个 articlecontroller 类中的方法edit';
+	}
+}
---
+++ app/home/controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * description		 测试类
+ *FILE_NAME          zxy_frame
+ *author             周天君
+ *date               2018/3/21 17:14:49
+ */
+
+namespace app\home\controller;
+
+
+class IndexController
+{
+public function index(){
+	echo '我是辅助测试app home controller 的一个类中的方法index';
+}
+	public function  edit(){
+		echo '我是辅助测试app home controller 的一个IndexController类中的方法edit';
+	}
+}
